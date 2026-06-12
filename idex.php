6ef818ffcc1e30a77b0727be8cba212dd38a785e
kevinriano007 <kevin.riano@cun.edu.co>
2026-06-12 14:50:48 -0500

    crear la clase persona

--- idex.php
+++ idex.php
@@ -1,3 +1,28 @@
 <?php
 
+ class persona {
+
+    public $nombre;
+    public $apellido;
+    public $edad;
+
+    function __construct($nombre, $apellido, $edad) 
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->edad = $edad;
+    }
+
+    function mostrar() 
+    {
+        echo "Nombre: " . $this->nombre . "<br>";
+        echo "Apellido: " . $this->apellido . "<br>"; 
+        echo "Edad: " . $this->edad . "<br>";
+    }
+
+ }
+
+$persona1 = new persona("Juan", "Pérez", 30);
+$persona1->mostrar();
+
 ?>
